009fad3ef1954034613de9cb61293ffa46236363
Harone Jaaved <jaaved23@hotmail.com>
2025-05-17 16:25:40 -0400

    updated code  file

--- haroneapp.py
+++ haroneapp.py
@@ -96,8 +96,10 @@ else:
  
 github = "https://github.com/haronejaaved"
 myprojects = "https://haronejaaved.github.io"
+csvfile = "https://codewithharone.com/wp-content/uploads/2025/05/cereal.csv.zip"
 
-
+st.markdown(f'<a href="{csvfile}" target="_blank">Csv file from Kaggle dataset</a>', unsafe_allow_html=True)
 st.markdown(f'<a href="{github}" target="_blank">Visit My Github Page</a>', unsafe_allow_html=True)
 st.markdown(f'<a href="{myprojects}" target="_blank">See some of my projects</a>', unsafe_allow_html=True)
+
 
